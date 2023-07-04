8f63c8519afb0306b02ded39ffc37a7ee7d71caa
gecleanme <130738860+gecleanme@users.noreply.github.com>
2023-07-04 15:41:50 +0300

    cleanup: reformat

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,49 @@
     <div class="flex w-screen h-screen">
 
         <div class="sidebar-container py-4 w-1/4 border-r-2 border-gray-300 flex flex-col flex-shrink-0 bg-gray-200">
-            <div v-if="showSuccess" class="success-msg flex space-x-2 text-white bg-green-500 rounded-full p-2 z-10 mb-2 bg-opacity-80">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+            <div v-if="showSuccess"
+                 class="success-msg flex space-x-2 text-white bg-green-500 rounded-full p-2 z-10 mb-2 bg-opacity-80">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
                 </svg>
 
                 <span class="pr-2">Success!</span>
             </div>
             <div class="sidebar-content">
                 <div class="flex  justify-between m-2 p-2">
-                <button @click="allNotes" class="text-gray-bg-gray-500 p-2 hover:bg-gray-100 cursor-pointer">All Notes</button>
-                 <button @click="newNote">
-                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                     </svg>
+                    <button @click="allNotes" class="text-gray-bg-gray-500 p-2 hover:bg-gray-100 cursor-pointer">All
+                        Notes
+                    </button>
+                    <button @click="newNote">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
 
-                 </button>
+                    </button>
                 </div>
                 <div class="flex justify-center items-center flex-col space-x-4 lg:overflow-y-scroll">
-                <a
-                    @click="showNote(note)"
-                    v-for="note in savedNotes"
-                    :key="note.date"
-                    class="p-2 mb-3 block hover:bg-gray-100 cursor-pointer "
-                    :class="{'bg-gray-100': selectedNote === note}">
-                    {{new Date(note.date).toLocaleString()}}
-                </a>
+                    <a
+                            @click="showNote(note)"
+                            v-for="note in savedNotes"
+                            :key="note.date"
+                            class="p-2 mb-3 block hover:bg-gray-100 cursor-pointer "
+                            :class="{'bg-gray-100': selectedNote === note}">
+                        {{ new Date(note.date).toLocaleString() }}
+                    </a>
                 </div>
             </div>
 
         </div>
-        <div v-if="clientOffline" class="offline-notice bg-red-500 opacity-80 text-center text-white w-1/4 h-[10%] z-10 absolute mb-2 flex items-center space-x-2 p-2">
+        <div v-if="clientOffline"
+             class="offline-notice bg-red-500 opacity-80 text-center text-white w-1/4 h-[10%] z-10 absolute mb-2 flex items-center space-x-2 p-2">
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-2 w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="ml-2 w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"/>
             </svg>
 
 
@@ -44,10 +53,13 @@
 
         </div>
 
-        <div v-if="!clientOffline && showOnline" class="offline-notice bg-green-500 opacity-80 text-center text-white w-1/4 h-[10%] z-10 absolute mb-2 flex items-center space-x-2 p-2">
+        <div v-if="!clientOffline && showOnline"
+             class="offline-notice bg-green-500 opacity-80 text-center text-white w-1/4 h-[10%] z-10 absolute mb-2 flex items-center space-x-2 p-2">
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-2 w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                 stroke="currentColor" class="ml-2 w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"/>
             </svg>
 
 
@@ -59,30 +71,35 @@
 
         <article class="ml-4 prose lg:prose-xl w-3/4 flex overflow-auto">
 
-            <Tiptap @note-content="grabHTML" :shown-note="shownNote" :clear-note="clearContent" />
+            <Tiptap @note-content="grabHTML" :shown-note="shownNote" :clear-note="clearContent"/>
             <div class="flex-grow"></div>
             <div class="btns absolute bottom-2 right-0 flex space-x-4">
-            <button @click="store" :disabled="!noteContent" class="mr-4 text-white rounded-full bg-green-500 outline-none hover:scale-110 disabled:bg-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 p-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3" />
-                </svg>
+                <button @click="store" :disabled="!noteContent"
+                        class="mr-4 text-white rounded-full bg-green-500 outline-none hover:scale-110 disabled:bg-gray-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="w-20 h-20 p-2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"/>
+                    </svg>
 
-            </button>
+                </button>
 
-            <button v-if="Object.keys(selectedNote).length" @click="deleteNote" class="text-white rounded-full bg-red-500 outline-none hover:scale-110">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 p-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                </svg>
+                <button v-if="Object.keys(selectedNote).length" @click="deleteNote"
+                        class="text-white rounded-full bg-red-500 outline-none hover:scale-110">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         stroke="currentColor" class="w-20 h-20 p-2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"/>
+                    </svg>
 
 
-            </button>
-                </div>
+                </button>
+            </div>
 
 
         </article>
 
     </div>
-
 
 
 </template>
@@ -107,7 +124,7 @@ window.addEventListener('online', () => {
         clientOffline.value = false;
         showOnline.value = true;
         setTimeout(() => {
-            showOnline.value= false;
+            showOnline.value = false;
         }, 4000);
 
     }
@@ -124,29 +141,29 @@ const grabHTML = (html) => {
 }
 
 async function initDB() {
-    return new Promise((resolve, reject)=>{
-        let db = window.indexedDB.open('hardnote',7);
-        db.onsuccess = e =>{
+    return new Promise((resolve, reject) => {
+        let db = window.indexedDB.open('hardnote', 7);
+        db.onsuccess = e => {
             resolve(e.target.result)
         }
 
-        db.onerror = e =>{
+        db.onerror = e => {
             reject('error on db init');
 
         }
 
-        db.onupgradeneeded = e =>{
-            e.target.result.createObjectStore('hardnote', {keyPath:"date"});
+        db.onupgradeneeded = e => {
+            e.target.result.createObjectStore('hardnote', {keyPath: "date"});
 
         }
     })
 }
 
-async function store(){
-    return new Promise((resolve, reject)=>{
+async function store() {
+    return new Promise((resolve, reject) => {
         let op = db.value.transaction('hardnote', 'readwrite').objectStore('hardnote');
 
-        op.onerror = e =>{
+        op.onerror = e => {
             reject('transaction error');
         };
 
@@ -172,8 +189,8 @@ async function store(){
 
                     resolve();
                     showSuccess.value = true;
-                    setTimeout(()=>{
-                        showSuccess.value=false;
+                    setTimeout(() => {
+                        showSuccess.value = false;
                     }, 4000)
                 };
                 updateNote.onerror = e => {
@@ -187,8 +204,8 @@ async function store(){
                     selectedNote.value = currentNote;
                     resolve();
                     showSuccess.value = true;
-                    setTimeout(()=>{
-                        showSuccess.value=false;
+                    setTimeout(() => {
+                        showSuccess.value = false;
                     }, 4000)
                 };
                 addNote.onerror = e => {
@@ -202,7 +219,7 @@ async function store(){
     })
 }
 
-async function grabNotes(){
+async function grabNotes() {
     return new Promise((resolve, reject) => {
         db.value.transaction('hardnote')
             .objectStore('hardnote')
@@ -214,33 +231,33 @@ async function grabNotes(){
 }
 
 
-function showNote(note){
+function showNote(note) {
     shownNote.value = note;
     selectedNote.value = note;
 }
 
-function allNotes(){
+function allNotes() {
 
     // cleanup
     clearContent.value = true
 
     selectedNote.value = {};
 
-    setTimeout(()=>{
+    setTimeout(() => {
         clearContent.value = false;
 
     }, 500)
 
 }
 
-function newNote(){
-    return new Promise((resolve, reject)=>{
+function newNote() {
+    return new Promise((resolve, reject) => {
         let op = db.value.transaction('hardnote', 'readwrite');
         op.oncomplete = e => {
             resolve();
             showSuccess.value = true;
-            setTimeout(()=>{
-                showSuccess.value=false;
+            setTimeout(() => {
+                showSuccess.value = false;
             }, 4000)
         }
 
@@ -268,11 +285,11 @@ function newNote(){
 
 //delete note here
 
-async function deleteNote(){
-    return new Promise((resolve, reject)=>{
+async function deleteNote() {
+    return new Promise((resolve, reject) => {
         let op = db.value.transaction('hardnote', 'readwrite').objectStore('hardnote');
 
-        op.onerror = e =>{
+        op.onerror = e => {
             reject('transaction error');
         };
 
@@ -299,7 +316,7 @@ async function deleteNote(){
 
                     selectedNote.value = {};
 
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         clearContent.value = false;
 
                     }, 500)
@@ -308,8 +325,8 @@ async function deleteNote(){
                     //window.location.reload();
 
                     showSuccess.value = true;
-                    setTimeout(()=>{
-                        showSuccess.value=false;
+                    setTimeout(() => {
+                        showSuccess.value = false;
                     }, 4000)
                 };
 
@@ -325,23 +342,23 @@ async function deleteNote(){
 }
 
 
-watchEffect(() =>{
+watchEffect(() => {
     if (noteContent.value === '<p></p>')
-        noteContent.value= null;
+        noteContent.value = null;
 })
 
 
-onMounted( async () => {
+onMounted(async () => {
 
     db.value = await initDB();
     let notes = await grabNotes();
-    savedNotes.value =notes.reverse();
+    savedNotes.value = notes.reverse();
 
 });
-    // watchEffect(() => {
-    // disableSave.value = !noteContent.value;
-    //
-    // });
+// watchEffect(() => {
+// disableSave.value = !noteContent.value;
+//
+// });
 </script>
 
 <style>
